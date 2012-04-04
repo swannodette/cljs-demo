d73fc514ff812056bf69d042d2caeb3641f79260
David Nolen <dnolen@Davids-MacBook-Pro.local>
2012-04-04 14:18:45 -0700

    * project.clj: bump deps

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
                  [domina "1.0.0-alpha2"]
                  [org.clojure/core.match "0.2.0-alpha9"]
                  [org.clojure/tools.macro "0.1.1"]
-                 [org.clojure/core.logic "0.7.0"]]
-  :dev-dependencies [[lein-cljsbuild "0.1.4"]]
+                 [org.clojure/core.logic "0.7.1"]]
+  :dev-dependencies [[lein-cljsbuild "0.1.6"]]
   :cljsbuild {:builds [{:source-path "src"
                         :compiler {:optimizations :simple
                                    :pretty-print true}}
